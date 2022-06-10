77d10fa515a1ff5a1ccd344ab0c4898a45f5a6e5
kkoehn <kristofer.koehn@wsu.edu>
2022-06-09 19:12:26 -0700

    changed player behavior

--- characters/Player.gd
+++ characters/Player.gd
@@ -1,26 +1,41 @@
 extends KinematicBody2D
 
-var thrust = 400
-var rotation_speed = 15
+var max_thrust = 400
+var velocity = Vector2(0,0)
+var velocity_damping = 1.02
+var acceleration = 15
+var rotation_speed = .9
+var rotation_max = 60
+var angular_velocity = 0
+var angular_damping = 1.15
 
 func _physics_process(delta: float) -> void:
-	var velocity = Vector2()
-	
+
 	if (Input.is_action_pressed("ui_left")):
-		velocity.x = -thrust
+		if self.angular_velocity <= -rotation_max:
+			self.angular_velocity = rotation_max
+		else:
+			self.angular_velocity -= rotation_speed
+		
 	if (Input.is_action_pressed("ui_right")):
-		velocity.x = thrust
+		if self.angular_velocity >= rotation_max:
+			self.angular_velocity = rotation_max
+		else:
+			self.angular_velocity += rotation_speed
 	if (Input.is_action_pressed("ui_up")):
-		velocity.y = -thrust
-	if (Input.is_action_pressed("ui_down")):
-		velocity.y = thrust
-	move_and_collide(velocity * delta)
+		if velocity.length() >= max_thrust:
+			velocity = velocity.normalized() * max_thrust
+		else:
+			velocity += self.get_rotation_vector() * acceleration
 	
-	rotation = 0.1
+	rotation += delta * angular_velocity
+	angular_velocity = angular_velocity/angular_damping
+	velocity = velocity / velocity_damping
+	move_and_collide(velocity * delta)
 
 func _unhandled_key_input(event):
 	if (event.is_action_pressed("shoot")):
 		$LaserWeapon.shoot()
 
 func get_rotation_vector():
-	return Vector2($RotationNode1.global_position.x - $RotationNode2.global_position.x, $RotationNode1.global_position.y - $RotationNode2.global_position.y)
+	return Vector2($RotationNode2.global_position.x - $RotationNode1.global_position.x, $RotationNode2.global_position.y - $RotationNode1.global_position.y)
--- objects/LaserWeapon.gd
+++ objects/LaserWeapon.gd
@@ -5,7 +5,7 @@ var laser_scene = load("res://objects/Laser.tscn")
 func shoot():
 	var laser = laser_scene.instance()
 	laser.global_position = self.global_position
-	laser._set_direction_vector(get_parent().get_rotation_vector())
+	laser._set_direction_vector(-get_parent().get_rotation_vector())
 	laser._set_rotation(get_parent().rotation)
 	get_node("/root/Game").add_child(laser)
 	
